9a5679678ee5260a200cd554858c88feb7d56ca3
WesleyHiromitsu <wes.hiro@gmail.com>
2020-08-12 09:03:51 -0300

    Added styled-component + globalstyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
 import Routes from './routes';
 
-const App: React.FC = () => <Routes />;
+const App: React.FC = () => (
+  <>
+    <Routes />
+    <GlobalStyle />
+  </>
+);
 
 export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components';
+
+import gitBg from '../assets/git_bg.svg';
+
+export default createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #F0f0f5 url(${gitBg}) no-repeat 70% top;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 16px Roboto, sans-serif;
+  }
+
+  #root {
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 40px 20px;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
